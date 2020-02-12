6194f12ebb3b915e725ca94663ae7c274163eee5
chrisantiz <santzch@gmail.com>
2020-02-11 20:22:48 -0500

    Remove types.d.ts

--- types.d.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultRootState } from 'react-redux';
-import { AnimalsState } from './src/store/animal/types';
-
-declare module 'react-redux/index' {
-  interface DefaultRootState {
-    animals: AnimalsState[];
-  }
-}
